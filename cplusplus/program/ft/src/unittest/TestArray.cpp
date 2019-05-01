3d698b9a9e8c7175492b2b59fec36904704414a0
aaakk <951412142@qq.com>
2019-05-01 16:51:38 +0800

    modify ft and gitignore

---
+++ cplusplus/program/ft/src/unittest/TestArray.cpp
@@ -0,0 +1,24 @@
+#include "TestArray.h"
+#include <iostream>
+
+FT_LIANG_BEG
+
+TestArray::TestArray()
+{
+}
+
+TestArray::~TestArray()
+{
+}
+
+void NullPointerA::Func1()
+{
+   std::cout << "1" ;
+}
+
+void NullPointerA::Func2()
+{
+   std::cout << "Run Func2!!" << std::endl;
+}
+
+FT_LIANG_END
